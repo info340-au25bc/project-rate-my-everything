af634626192db1287778c664dbe3a110f1d3f99a
ianhu <ianhutchings77@gmail.com>
2025-11-19 01:57:47 -0800

    finished adding components for home page

--- project-draft/src/components/App.jsx
+++ project-draft/src/components/App.jsx
@@ -2,28 +2,29 @@ import React from 'react';
 import { NavBar } from './NavBar';
 import { Footer } from './Footer';
 import { RecCards } from './RecCards';
+import LOG_DATA from '../data/logs.json';
 
 
 function App() {
     return (
-        <div className='container'>
+        <div>
             <header>
                 < NavBar />
             </header>
 
             <main>
-                <div class="header-container">
-                    <div class="image-row">
+                <div className="header-container">
+                    <div className="image-row">
                         <img src="img/study1.jpg" alt="indoor view of a library with cherry blossoms outside of the window" />
                         <img src="img/study2.jpg" alt="indoor vieww of a public area in the PACCAR building" />
                         <img src="img/theAve.jpeg" alt="a street view of an orange building with graffities on it" />
                     </div>
-                    <div class="overlay-text">
+                    <div className="overlay-text">
                         <h1>Rate My Everything</h1>
                     </div>
                 </div>
 
-                <div class="intro container">
+                <div className="intro container">
                     <p>
                         Come join the community where you get to share and rate everything around you! It could
                         be a spot you found on campus that you enjoy studying at or a favorite restaurant you have
@@ -32,14 +33,7 @@ function App() {
                     </p>
                 </div>
 
-                <div class="container log-section">
-                    <h2 class="log-title">Daily Recommended Logs</h2>
-                </div>
-
-                <div class="rec-log container py-5 row g-4"> 
-                    < RecCards />
-                </div>
-                
+                <RecCards data={LOG_DATA} />
 
             </main>
 
--- project-draft/src/components/RecCards.jsx
+++ project-draft/src/components/RecCards.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 
-export function RecCards() {
+export function RecCards({ data }) {
+    const recCards = data.map(log => <RecCard key={log.name} logData={log} />)
+
     return (
-        <div class="col-12 col-md-6 col-lg-4">
-            <div class="card horizontal-card card h-100">
-                <img class="card-img-left" src="img/ugly.jpg" alt="a picture of a cafe indoor" />
-                <div class="card-body d-flex flex-column text-center justify-content-center">
-                    <h2 class="card-title fs-5 fs-md-4">Recommended Study Space - Ugly Mug Cafe</h2>
-                    <p class="card-text fs-6 fs-md-5">❤️❤️❤️🤍🤍</p>
-                    <button class="btn btn-dark w-100 desc-btn">Review Description</button>
-                    <button class="btn btn-dark w-100 add-list">Add to List</button>
+        <div className="rec-log container py-5"> 
+            <h2 className="log-title">Daily Recommended Logs</h2>
+            <div className="row g-4">
+                {recCards}
+            </div>
+        </div>
+    )
+}
+
+function RecCard({ logData }) {
+    return (
+        <div className="col-12 col-md-6 col-lg-4">
+            <div className="card horizontal-card card h-100">
+                <img className="card-img-left" src={logData.img} alt="a picture of a cafe indoor" />
+                <div className="card-body d-flex flex-column text-center justify-content-center">
+                    <h2 className="card-title fs-5 fs-md-4">Recommended {logData.category} - {logData.name}</h2>
+                    <p className="card-text fs-6 fs-md-5">{logData.rating}/5</p>
+                    <button className="btn btn-dark w-100 desc-btn">Review Description</button>
+                    <button className="btn btn-dark w-100 add-list">Add to List</button>
                 </div>
             </div>
         </div>
